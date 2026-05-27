b050b8f8ced71e2986fd517e4b3496e4dec52dff
Lucas Rimbano <Lucasrimbano@gmail.com>
2026-05-27 12:42:27 -0300

    Creando repositorio para practica de java con fines academicos

---
+++ listanombres.java
@@ -0,0 +1,11 @@
+public class listanombres {
+    public static void main (String[] args){
+        String[] nombres = {"Lucas" , "Juan" ,"Maria" , "Estela", "Mario"};
+
+        for(String nombre: nombres){   //creo una variable temporal llamada nombre y recorro el string [] nombres , nombre= lucas ,nombre = juan ....
+            System.out.println(nombre);
+        }
+
+
+    }
+}
---
+++ nombrelistasconfilter.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class nombrelistasconfilter {
+    public static void main (String[] arg){
+
+        ArrayList<String> nombres = new ArrayList<>();
+            nombres.add("Lucas");
+            nombres.add("Mario");
+            nombres.add("Maria");
+            nombres.add("Julieta");
+            nombres.add("Juan");
+            nombres.add("Santiago");
+            nombres.add("Lorena");
+
+            nombres.stream()
+                .filter(nombre -> nombre.startsWith("J"))
+                .forEach(nombre -> System.out.println(nombre));
+
+            
+            System.out.println("El tamaño de la lista es de:" + nombres.size());
+            System.out.println("Primer elemento de la lista:" + nombres.get (0));
+            System.out.println("Ultimo elemento de la lista:" + nombres.get(nombres.size()-1));
+
+            
+            System.out.println("Nombres ordenados alfabeticamente:");
+            nombres.stream()
+                .sorted()
+                .forEach(nombre -> System.out.println(nombre));
+
+
+            long cantidad = nombres.stream()
+                .filter(nombre-> nombre.startsWith("M"))
+                .count();
+
+            if (cantidad == 0){
+                System.out.println("No se encontro ningun nombre con M,lo siento :C");
+            }   else{
+                System.out.println("Cantidad de nombres que empiezan con M son:" + cantidad);
+              
+            }
+                    
+            nombres.stream()
+                .filter(nombre -> nombre.startsWith("M"))
+                .forEach(nombre -> System.out.println("Los nombres que empiezan con M son:" +nombre));
+            
+            
+            
+
+            nombres.stream()
+                .map(nombre -> nombre.toLowerCase())
+                .forEach(nombre->System.out.println("Nombres pasados a minuscula:"+ nombre));
+
+
+            }     
+            
+            
+    }
